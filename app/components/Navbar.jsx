f0c95ca325f4f05be31c7c42addc49732e0e70b9
Aly Khayrulina <a.a.khayrulina@gmail.com>
2017-03-22 11:53:24 -0400

    log in splash and logout works

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-export default function Navbar() {
-
+export default function Navbar(props) {
+  const user = props.user
   return (
       <nav className ="navbar navbar-inverse navbar-fixed-top" role="navigation">
           <div className ="navbar-header">
@@ -17,23 +17,19 @@ export default function Navbar() {
           <div className ="collapse navbar-collapse navbar-ex1-collapse">
             <ul className ="nav navbar-right top-nav">
                 <li className ="dropdown">
-                    <a href="#" className ="dropdown-toggle" data-toggle="dropdown">FIRST NAME <b className ="caret"></b></a>
+                    <a href="#" className ="dropdown-toggle" data-toggle="dropdown">Welcome back, {user.name || user.email}<b className ="caret"></b></a>
                     <ul className ="dropdown-menu">
                         <li>
                             <a href="#">Settings</a>
                         </li>
                         <li className ="divider"></li>
                         <li>
-                            <a href="#">Logout</a>
+                            <a href="#" onClick={props.handleLogout}>Logout</a>
                         </li>
                     </ul>
                 </li>
             </ul>
           </div>
-
       </nav>
-
-
     )
-
 }
--- app/containers/AppContainer.jsx
+++ app/containers/AppContainer.jsx
@@ -1,16 +1,38 @@
 import React from 'react';
+import {connect} from 'react-redux'
 import Sidebar from '../components/Sidebar'
 import Navbar from '../components/Navbar'
+import Login from '../components/Login'
+import {logout} from '../reducers/auth'
 
-export default function(props) {
-
+export default connect(
+  (state) => {
+    return {
+      user: state.auth
+    }
+  },
+  (dispatch) => {
+    return {
+      handleLogout: () => {
+        dispatch(logout())
+      }
+    }
+  }
+)(function(props) {
   return (
     <div style={{display: 'flex'}}>
-      <Navbar />
-      <Sidebar />
-      <div id="main-content">
-      { props.children && React.cloneElement(props.children, props) }
-      </div>
+      { props.user ?
+      <div>
+        <Navbar user={props.user} handleLogout={props.handleLogout}/>
+        <Sidebar />
+        <div id="main-content">
+        { props.children && React.cloneElement(props.children, props) }
+        </div>
+      </div> : <Login />
+      }
     </div>
   )
-}
+})
+
+
+
