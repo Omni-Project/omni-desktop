60cc98ea15abab1271069ae2b88769b5bdb67ca0
Chloe Hwang <c.chloeh@gmail.com>
2017-03-23 18:03:32 -0400

    working on anims

--- app/components/Sky.jsx
+++ app/components/Sky.jsx
@@ -6,4 +6,7 @@ export default props => (
     geometry={{primitive: 'sphere', radius: 100}}
     material={{shader: 'flat', color: '#242424'}}
     scale="1 1 -1"/>
+
+
+    //<a-sky src="#sky"></a-sky>
 );
--- app/components/SpriteScene.jsx
+++ app/components/SpriteScene.jsx
@@ -6,19 +6,34 @@ import Sky from './Sky'
 export default class VRScene extends React.Component {
   render () {
     return (
-      <a-scene>
+      <a-scene fog="type: linear; color: #AAA">
         <Sky />
 
         <a-assets>
-          <a-asset-item id="surprise" src="surprise.obj"></a-asset-item>
-          <a-asset-item id="joy" src="joy.obj"></a-asset-item>
-          <a-asset-item id="sadness" src="sadness.obj"></a-asset-item>
-          <a-asset-item id="anger" src="anger.obj"></a-asset-item>
-          <a-asset-item id="fear" src="fear.obj"></a-asset-item>
+          <a-asset-item id="surprise" src="./objects/surprise.obj"></a-asset-item>
+          <a-asset-item id="joy" src="./objects/joy.obj"></a-asset-item>
+          <a-asset-item id="sadness" src="./objects/sadness.obj"></a-asset-item>
+          <a-asset-item id="anger" src="./objects/anger.obj"></a-asset-item>
+          <a-asset-item id="fear" src="./objects/fear.obj"></a-asset-item>
+          <img id="sky" src="/env-images/1.jpg" />
+
+          {/** TEXTURES **/}
+          <img id="plastic" src="/textures/plastic.jpg" />
+          <img id="rust" src="/textures/yellow-009.jpg" />
+
+          <a-asset-item id="floor-obj" src="./objects/floor.obj"></a-asset-item>
         </a-assets>
 
+
+
+{/*<a-entity obj-model="obj: #sadness;" material="src: #plastic; metalness: 0.6" id="floor"></a-entity>*/}
+
+
+
+
+
         {/** SURPRISE **/}
-        <a-obj-model src="#surprise" material="color: white" position="0 -2 -7" scale="0 0 0">
+        <a-obj-model src="#surprise" material="src: #plastic; metalness: 0.6" position="0 -2 -7" scale="0 0 0">
           <a-animation
             attribute="scale"
             to='2 2 2'
@@ -28,9 +43,9 @@ export default class VRScene extends React.Component {
             repeat="indefinite" />
           <a-animation
             attribute="color"
-            from="white"
-            to="red"
-            dur="500"
+            from="#ff7da7"
+            to="#f02b6b"
+            dur="700"
             easing="ease-in"
             direction="alternate"
             repeat="indefinite"
@@ -52,7 +67,7 @@ export default class VRScene extends React.Component {
         </a-obj-model>
 
         {/** JOY **/}
-        <a-obj-model src="#joy" position="0 -1 -7" material="color: #fff6a5" scale="0.6 0.6 0.6">
+        <a-obj-model src="#joy" position="0 -1 -7" material="src: #rust; color: #696969; roughness: 0; metalness: 0.3" >
           <a-animation
             attribute="rotation"
             to='360 360 0'
@@ -61,16 +76,16 @@ export default class VRScene extends React.Component {
             dur="4000"
             repeat="indefinite" />
 
-          <a-animation
+          {/*<a-animation
             attribute="scale"
             to="1.1 1.1 1.1"
             direction="alternate"
             dur="4000"
-            repeat="indefinite" />
+            repeat="indefinite" />*/}
         </a-obj-model>
 
         {/** ANGER **/}
-        <a-obj-model src="#anger" position="0 -3 -7" scale="0.8 0.8 0.8" material="color: #ff7a7a">
+        <a-obj-model src="#anger" position="0 -3.5 -7" scale="0.8 0.8 0.8" material="color: #a80500;  metalness:1; roughness: 0; sphericalEnvMap: #sky;">
           <a-animation
             attribute="rotation"
             to='0 0 -45'
@@ -80,8 +95,18 @@ export default class VRScene extends React.Component {
             repeat="indefinite" />
         </a-obj-model>
 
+         <a-obj-model src="#anger" rotation="0 45 0" position="0 -3.5 -7" scale="0.8 0.8 0.8" material="color: #a80500;  metalness:1; roughness: 0; sphericalEnvMap: #sky;">
+          <a-animation
+            attribute="rotation"
+            to='0 45 -45'
+            dur="1000"
+            direction="alternate"
+            easing="ease-out-back"
+            repeat="indefinite" />
+        </a-obj-model>
 
-        <a-obj-model src="#anger" rotation="0 180 0" position="0 -3 -7" scale="0.8 0.8 0.8" material="color: #ff7a7a">
+
+        <a-obj-model src="#anger" rotation="0 180 0" position="0 -3.5 -7" scale="0.8 0.8 0.8" material="color: #a80500;  metalness:1; roughness: 0; sphericalEnvMap: #sky;">
           <a-animation
             attribute="rotation"
             to='0 180 -45'
@@ -91,21 +116,61 @@ export default class VRScene extends React.Component {
             repeat="indefinite" />
         </a-obj-model>
 
-        {/** SADNESS **/}
-        <a-obj-model src="#sadness" position="0 -3 -7"  material="color: #a5dcff">
+        <a-obj-model src="#anger" rotation="0 225 0" position="0 -3.5 -7" scale="0.8 0.8 0.8" material="color: #a80500;  metalness:1; roughness: 0; sphericalEnvMap: #sky;">
           <a-animation
             attribute="rotation"
-            to='0 360 0'
-            dur="5000"
-            fill="forwards"
+            to='0 225 -45'
+            dur="1000"
+            direction="alternate"
+            easing="ease-out-back"
             repeat="indefinite" />
         </a-obj-model>
 
+        {/** SADNESS **/}
+        <a-obj-model src="#sadness" position="0 -3 -7" material="src: #plastic; color: rgb(159, 190, 221);">
+          <a-animation
+            easing="linear"
+            attribute="rotation"
+            dur="5000"
+            to="0 360 0"
+            repeat="indefinite"></a-animation>
+
+          <a-animation
+            attribute="opacity"
+            from="0.8"
+            to="0.5"
+            dur="2000"
+            easing="ease-in-out"
+            direction="alternate"
+            repeat="indefinite"
+          />
+        </a-obj-model>
+
         {/** LIGHTS **/}
-        <a-entity light ="color:#AFA; intensity: 5" position="0 -2 -7" ></a-entity>
-        <a-entity light="type: ambient; color: #BBB"></a-entity>
-        <a-entity light="type: directional; color: #FFF; intensity: 0" position="-0.5 1 1"></a-entity>
+
+        {/*purplish light*/}
+        <a-entity light="color: #ffaaff; intensity: 3" position="5 5 5"></a-entity>
+
+        {/*ambient light*/}
+        <a-entity light="color: white; type: ambient;"></a-entity>
+
+        {/*light inside of joy*/}
+        <a-entity light="color: #94c6ff; distance: 15; intensity: 7; type: point" position="0 -1 -7"></a-entity>
+
+
       </a-scene>
     );
   }
 }
+
+
+/*
+LIGHT EXAMPLES
+
+<a-entity light ="color:#AFA; intensity: 5" position="0 -2 -7" ></a-entity>
+<a-entity light="type: ambient; color: #fff"></a-entity>
+<a-entity light="type: directional; color: #FFF; intensity: 0" position="-0.5 1 1"></a-entity>*/
+/*<a-entity light="color: white; intensity: 0.5" position="-5 5 15"></a-entity>
+<a-entity light="color: white; type: ambient;"></a-entity>
+
+<a-entity light="type: hemisphere; color: #33C; groundColor: #3C3; intensity: 2"></a-entity>*/
