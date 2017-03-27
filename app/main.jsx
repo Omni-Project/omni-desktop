95facbdea4e89d574943bc60c027f78da2d81609
Aly Khayrulina <a.a.khayrulina@gmail.com>
2017-03-27 15:51:50 -0400

    fixed on enter hooks

--- app/main.jsx
+++ app/main.jsx
@@ -6,8 +6,9 @@ import { Provider } from 'react-redux'
 import axios from 'axios'
 
 import store from './store'
-import { selectDream, getPublicDreams } from './reducers/dreams'
+import { selectDream, getPublicDreams, fetchAllDreams } from './reducers/dreams'
 import { fetchWeekAnalytics, fetchUser } from './reducers/analytics'
+import { authenticated } from './reducers/auth'
 
 import Login from './components/Login'
 import WhoAmI from './components/WhoAmI'
@@ -18,6 +19,27 @@ import SingleDream from './components/SingleDream'
 import AppContainer from './containers/AppContainer'
 import DreamsContainer from './containers/DreamsContainer'
 import AnalyticsContainer from './containers/AnalyticsContainer'
+
+function onAppEnter (nextRouterState, replace, done){
+//do not enter app until you get user - making async call here to use the done()
+//fixes id errors we were having in other onEnter hooks (user not on state yet, but hook being called with store.getState().auth)
+  axios.get('/api/auth/whoami')
+      .then(response => {
+        const user = response.data
+        store.dispatch(authenticated(user))
+        if (user) store.dispatch(fetchAllDreams(user.id))
+        return done()
+      })
+      .catch(failed => {
+        store.dispatch(authenticated(null))
+        return done()
+      })
+}
+
+function onDreamsEnter(nextRouterState){
+  const user = store.getState().auth
+  store.dispatch(fetchAllDreams(user.id))
+}
 
 function onSingleDreamEnter(nextRouterState, replace, done){
   const dreamId = nextRouterState.params.id
@@ -50,20 +72,18 @@ function onPublicDreamsEnter(nextRouterState, replace, done){
 render (
   <Provider store={store}>
     <Router history={browserHistory}>
-
-    <Route path="/" component={AppContainer} >
-    <Route path="public" component={AllSpritesView} onEnter={onPublicDreamsEnter} />
+    <Route path="/" component={AppContainer} onEnter={onAppEnter}>
+      <Route path="public" component={AllSpritesView} onEnter={onPublicDreamsEnter} />
       <IndexRedirect to="/dreams" />
-      <Route path="dreams" component={DreamsContainer} >
+      <Route path="dreams" component={DreamsContainer} onEnter={onDreamsEnter}>
         <IndexRedirect to="/dreams/all" />
         <Route path="all" component={AllDreams} />
         <Route path="add" component={AddDreamForm} />
         <Route path=":id" component={SingleDream} onEnter={onSingleDreamEnter} />
       </Route>
       <Route path="analytics" component={AnalyticsContainer} onEnter={fetchAnalytics} />
-
-
     </Route>
+    <Route path="/login" component={Login} />
     </Router>
   </Provider>,
   document.getElementById('main')
